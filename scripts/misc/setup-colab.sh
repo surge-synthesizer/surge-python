b717ebcdef3671c83b8f33e69d48352eedc7b853
Joseph Turian <turian@gmail.com>
2020-12-21 20:35:31 +0100

    Remove redirects to /dev/null

--- scripts/misc/setup-colab.sh
+++ scripts/misc/setup-colab.sh
@@ -9,11 +9,11 @@ apt-get -qq install build-essential libcairo-dev libxkbcommon-x11-dev libxkbcomm
 # We need cmake >= 3.15
 apt-get -qq remove cmake
 apt -qq autoremove
-apt-get -qq install -y apt-transport-https ca-certificates gnupg software-properties-common wget > /dev/null
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -
-apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main' > /dev/null
+apt-get -qq install -y apt-transport-https ca-certificates gnupg software-properties-common wget
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc | apt-key add -
+apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
 apt-get -qq update
-apt-get -qq install cmake > /dev/null
+apt-get -qq install cmake
 
 # Clone surge from master, and build
 git clone --quiet https://github.com/surge-synthesizer/surge.git
@@ -25,5 +25,5 @@ git submodule --quiet update --init --recursive
 # We use colab specific paths to find -lpython, but please modify
 # this if your python.so is in another directory.
 
-LD_LIBRARY_PATH="/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu/:$LD_LIBRARY_PATH" /usr/bin/cmake --build buildpy --config Release --target surgepy > /dev/null
+LD_LIBRARY_PATH="/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu/:$LD_LIBRARY_PATH" /usr/bin/cmake --build buildpy --config Release --target surgepy
 /usr/bin/cmake --build buildpy --target install-resources-local
